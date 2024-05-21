4b28e1a2e6126f7a777ef1e009814663e076be22
Sutomo <yogasutomo67@gmail.com>
2024-05-21 19:21:57 +0700

    Push code to remote repository

---
+++ src/server/routes.js
@@ -0,0 +1,19 @@
+const { max } = require('@tensorflow/tfjs-node');
+const postPredictHandler = require('../server/handler');
+
+const routes = [
+    {
+      path: '/predict',
+      method: 'POST',
+      handler: postPredictHandler,
+      options: {
+        payload: {
+          allow: 'multipart/form-data',
+          multipart: true,
+          maxBytes: 1000000
+        }
+      }
+    }
+  ]
+   
+  module.exports = routes;
