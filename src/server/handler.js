1bbe926be78edb7365bcf5bdbfa7c46182b2614f
Sutomo <yogasutomo67@gmail.com>
2024-05-21 21:41:07 +0700

    Adding method get

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,6 +1,9 @@
+const { get } = require('http');
 const predictClassification = require('../services/inferenceService');
 const storeData = require('../services/storeData');
+const { Firestore } = require('@google-cloud/firestore');
 const crypto = require('crypto');
+const { data } = require('@tensorflow/tfjs-node');
 
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
@@ -28,4 +31,37 @@ async function postPredictHandler(request, h) {
     return response;
 }
 
-module.exports = postPredictHandler;
+const firestore = new Firestore();
+
+const getPredictHandler = async (_request, h) => {
+    try {
+        const histories = [];
+        const snapshot = await firestore.collection('prediction').get();
+        
+        snapshot.forEach(doc => {
+            histories.push(doc.data());
+        });
+        
+        const formattedHistories = histories.map(history => ({
+            id: history.id,
+            history: {
+                result: history.result,
+                createdAt: history.createdAt,
+                suggestion: history.suggestion,
+                id: history.id
+            }
+        }));
+
+        return h.response({
+            status: 'success',
+            data: formattedHistories
+        }).code(200);
+    } catch (error) {
+        return h.response({
+            status: 'fail',
+            message: error.message
+        }).code(500);
+    }
+};
+
+module.exports = {postPredictHandler, getPredictHandler};
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,18 +1,24 @@
 const { max } = require('@tensorflow/tfjs-node');
-const postPredictHandler = require('../server/handler');
+const {postPredictHandler, getPredictHandler} = require('../server/handler');
 
 const routes = [
     {
-      path: '/predict',
-      method: 'POST',
-      handler: postPredictHandler,
-      options: {
-        payload: {
-          allow: 'multipart/form-data',
-          multipart: true,
-          maxBytes: 1000000
+
+        path: '/predict',
+        method: 'POST',
+        handler: postPredictHandler,
+        options: {
+            payload: {
+            allow: 'multipart/form-data',
+            multipart: true,
+            maxBytes: 1000000
+            }
         }
-      }
+    },
+    {
+        path: '/predict/histories',
+        method: 'GET',
+        handler: getPredictHandler,
     }
   ]
    
